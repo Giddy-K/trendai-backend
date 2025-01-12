0fdb1c85c4b6ed9dd949a6d44bdab6fb729c2112
Giddy-K <gideonkipamet@gmail.com>
2025-01-12 19:58:54 +0300

    Minor changes on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CampaignsModule } from './campaigns/campaigns.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Campaign, CampaignSchema } from './schemas/campaign.schema';
+import { Metrics, MetricsSchema } from './schemas/metrics.schema';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forFeature([
+      { name: Campaign.name, schema: CampaignSchema },
+      { name: Metrics.name, schema: MetricsSchema },
+    ]),
+    CampaignsModule,
+    MetricsModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
